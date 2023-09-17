f7fcd0a6db99d103dec5d92e5d4d161011badda3
rhtm123 <maurya.iitk@gmail.com>
2023-09-17 11:05:24 +0530

    breadcrumbs on self-paced

--- pages/self-paced-courses.js
+++ pages/self-paced-courses.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useAppContext } from "@/context/state";
 import Course from "@/components/Course";
 import Head from "next/head";
+import Link from "next/link";
 
 function SelfPacedCoursesPage() {
 
@@ -33,7 +34,7 @@ function SelfPacedCoursesPage() {
 
         <div className="text-sm breadcrumbs">
   <ul>
-  <li>
+    <li>
       <Link href={"/"}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-2 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
         Home
